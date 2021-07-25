6dbceba4096748a726bddae75f74d46f009ff344
Nar <51009725+narvmtz@users.noreply.github.com>
2021-07-25 12:48:49 -0500

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'ecommerce';
+  title = 'ToDo list';
+  item = '';
+  items: string[] = [];
+
+  addItem(item: string) {
+    this.items.push(item);
+    this.item = '';
+  }
+
+  deleteItem(index: number) {
+    this.items.splice(index, 1);
+  }
+
+  deleteAll(): void {
+    this.items = [];
+  }
 }
