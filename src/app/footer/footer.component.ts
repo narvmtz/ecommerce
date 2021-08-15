31d506d38e45853ea6fbbe2f09fdfaa2af4dbc65
Nar <51009725+narvmtz@users.noreply.github.com>
2021-08-14 19:54:56 -0500

    Add header & footer component to home page

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { faGithub, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
+import { faLink } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
+  faGithub = faGithub;
+  faLinkedin = faLinkedinIn;
+  faLink = faLink;
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+
+import Swiper from 'swiper/bundle';
+import SwiperCore, { Autoplay, Navigation, Pagination } from 'swiper/core';
+// configure Swiper to use modules
+SwiperCore.use([Autoplay, Pagination, Navigation]);
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
+  swiper!: Swiper;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  ngAfterViewInit() {
+    this.swiper = new Swiper('.swiper-container');
   }
-
 }
