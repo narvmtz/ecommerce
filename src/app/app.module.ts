8f87f15abb2d928c136330773fb8f2ca9ba24ac0
Nar <51009725+narvmtz@users.noreply.github.com>
2021-08-14 19:52:48 -0500

    Add banner component & some styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SwiperModule } from 'swiper/angular';
 
 import { CardComponent } from './components/card.component';
 import { FilterPipe } from './pipes/filter.pipe';
@@ -13,10 +14,21 @@ import { TodoComponent } from './todo/todo.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { BannerComponent } from './banner/banner.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
-  declarations: [AppComponent, CardComponent, FilterPipe, HomeComponent, CardsComponent, TodoComponent, HeaderComponent, FooterComponent, BannerComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  declarations: [
+    AppComponent,
+    CardComponent,
+    FilterPipe,
+    HomeComponent,
+    CardsComponent,
+    TodoComponent,
+    HeaderComponent,
+    FooterComponent,
+    BannerComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, SwiperModule, FontAwesomeModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/banner/banner.component.ts
+++ src/app/banner/banner.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Banner } from './banner.model';
+
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
-  styleUrls: ['./banner.component.scss']
+  styleUrls: ['./banner.component.scss'],
 })
 export class BannerComponent implements OnInit {
+  images: Banner[] = [
+    { imgUrl: 'https://media.wizards.com/2016/images/daily/WkrSeoBmCN.jpg' },
+    {
+      imgUrl:
+        'https://as.com/meristation/imagenes/2020/09/03/noticias/1599127190_301697_1599127352_noticia_normal.jpg',
+    },
+    {
+      imgUrl:
+        'https://i.pinimg.com/originals/f4/64/fd/f464fd3604d3ad8c1813d1f6731caa3f.jpg',
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onSwiper(swiper: any) {
+    console.log(swiper);
   }
-
+  onSlideChange() {
+    console.log('slide change');
+  }
 }
